ab97959e3e53660e697eb3a9b2deb9eaf412d9aa
tinysec <root@tinysec.net>
2025-11-27 14:47:38 +0800

    feat: add equals for InstructionInfo

--- Struct/BNInstructionInfo.cs
+++ Struct/BNInstructionInfo.cs
@@ -53,7 +53,9 @@ namespace BinaryNinja
 		internal IntPtr branchArch_2;
 	}
 	
-	public sealed class InstructionBranch
+	public sealed class InstructionBranch :
+		IEquatable<InstructionBranch>, 
+		IComparable<InstructionBranch>
 	{
 		public BranchType Type { get; } = BranchType.UnconditionalBranch;
 
@@ -71,9 +73,102 @@ namespace BinaryNinja
 			this.Address = address;
 			this.Architecture = arch;
 		}
+		
+		public override bool Equals(object? other)
+		{
+			return this.Equals(other as InstructionBranch);
+		}
+
+		public bool Equals(InstructionBranch? other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this , other))
+			{
+				return true;
+			}
+
+			if (this.Type != other.Type)
+			{
+				return false;
+			}
+
+			if (this.Address != other.Address)
+			{
+				return false;
+			}
+			
+			return this.Architecture == other.Architecture;
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2218:OverrideGetHashCodeOnOverridingEquals")]
+		public override int GetHashCode()
+		{
+			return HashCode.Combine<uint,ulong,int>( 
+				(uint)this.Type,
+				(ulong)this.Address,
+				null == this.Architecture ? 0 : this.Architecture.GetHashCode()
+			);
+		}
+
+		public static bool operator ==(InstructionBranch? left, InstructionBranch? right)
+		{
+			if (left is null)
+			{
+				return right is null;
+			}
+			
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(InstructionBranch? left, InstructionBranch? right)
+		{
+			return !(left == right);
+		}
+
+		public int CompareTo(InstructionBranch? other)
+		{
+			if (other is null)
+			{
+				return 1;
+			}
+			
+			int result = this.Type.CompareTo(other.Type);
+
+			if (0 == result)
+			{
+				result = this.Address.CompareTo(other.Address);
+			}
+
+			if (0 == result)
+			{
+				if (null == this.Architecture)
+				{
+					if (null == other.Architecture)
+					{
+						result = 0;
+					}
+					else
+					{
+						result = -1;
+					}
+				}
+				else
+				{
+					result = this.Architecture.CompareTo(other.Architecture);
+				}
+			}
+			
+			return result;
+		}
 	}
 	
-    public sealed class InstructionInfo 
+    public sealed class InstructionInfo :
+	    IEquatable<InstructionInfo>, 
+	    IComparable<InstructionInfo>
     {	
 		public ulong Length { get;} = 0;
 		
@@ -130,5 +225,134 @@ namespace BinaryNinja
 		{
 			return new InstructionInfo(native);
 		}
+		
+		public override bool Equals(object? other)
+		{
+			return this.Equals(other as InstructionInfo);
+		}
+
+		public bool Equals(InstructionInfo? other)
+		{
+			if (other is null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this , other))
+			{
+				return true;
+			}
+
+			if (this.Length != other.Length)
+			{
+				return false;
+			}
+			
+			if (this.ArchTransitionByTargetAddr != other.ArchTransitionByTargetAddr)
+			{
+				return false;
+			}
+			
+			if (this.DelaySlots != other.DelaySlots)
+			{
+				return false;
+			}
+			
+			if (this.Branches.Length != other.Branches.Length)
+			{
+				return false;
+			}
+
+			for (int i = 0; i < this.Branches.Length; i++)
+			{
+				if (this.Branches[i] != other.Branches[i])
+				{
+					return false;
+				}
+			}
+			
+			return true;
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2218:OverrideGetHashCodeOnOverridingEquals")]
+		public override int GetHashCode()
+		{
+			return HashCode.Combine<ulong,bool,byte,int>( 
+				this.Length,
+				this.ArchTransitionByTargetAddr,
+				this.DelaySlots,
+				this.Branches.GetHashCode()
+			);
+		}
+
+		public static bool operator ==(InstructionInfo? left, InstructionInfo? right)
+		{
+			if (left is null)
+			{
+				return right is null;
+			}
+			
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(InstructionInfo? left, InstructionInfo? right)
+		{
+			return !(left == right);
+		}
+
+		public int CompareTo(InstructionInfo? other)
+		{
+			if (other is null)
+			{
+				return 1;
+			}
+			
+			int result = this.Length.CompareTo(other.Length);
+
+			if (0 == result)
+			{
+				result = this.ArchTransitionByTargetAddr.CompareTo(other.ArchTransitionByTargetAddr);
+			}
+
+			if (0 == result)
+			{
+				result = this.DelaySlots.CompareTo(other.DelaySlots);
+			}
+
+			if (0 == result)
+			{
+				if (this.Branches.Length <= other.Branches.Length)
+				{
+					return -1;
+				}
+				else if (this.Branches.Length > other.Branches.Length)
+				{
+					return 1;
+				}
+				else
+				{
+					for (int i = 0; i < this.Branches.Length; i++)
+					{
+						int code = this.Branches[i].CompareTo(other.Branches[i]);
+						
+						if ( code < 0)
+						{
+							return -1;
+						}
+						else if ( code > 0 )
+						{
+							return 1;
+						}
+						else
+						{
+							
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+		
     }
 }
